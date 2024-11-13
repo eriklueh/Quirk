862fe1a7be27d554e43c19581675b234e19d8427
Erik <eriklueh@nuvadi.com>
2024-11-13 01:39:01 -0300

    ,

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server';
 import nodemailer from 'nodemailer';
 
 export async function POST(request: Request) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const { name, email, message } = await request.json();
 
     const transporter = nodemailer.createTransport({
